72fc286b7609e369e56a96ae1e19597f25a3de29
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-03-16 13:07:44 +0100

    create function onDrag

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [drag, setDrag] = useState(true)
+    const [drag, setDrag] = useState(false)
 
-  return (
-    <>
+    function dragStartHundler(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault()
+        setDrag(true)
+    }
 
-    </>
-  )
+    function dragLeavetHundler(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault()
+        setDrag(false)
+    }
+
+    return (
+        <div className='app'>
+            {drag
+                ? <div
+                    onDragStart={(e => dragStartHundler(e))}
+                    onDragLeave={(e => dragLeavetHundler(e))}
+                    onDragOver={(e => dragStartHundler(e))}
+                    className='drop-area'>Отпусти файлы, чтобы загрузить их</div>
+                : <div
+                    onDragStart={(e => dragStartHundler(e))}
+                    onDragLeave={(e => dragLeavetHundler(e))}
+                    onDragOver={(e => dragStartHundler(e))}
+                >Перетащи файлы, чтобы загрузить их</div>
+            }
+
+        </div>
+    )
 }
 
 export default App
