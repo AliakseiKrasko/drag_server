dad1319763f4d028f76d3b0e661bd9c4941b7547
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-03-16 13:34:07 +0100

    finished all project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
     const [drag, setDrag] = useState(false)
@@ -14,6 +15,28 @@ function App() {
         setDrag(false)
     }
 
+    function dropHundler(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault()
+        const files = [...e.dataTransfer.files]
+        const formData = new FormData()
+        formData.append('file', files[0])
+        formData.append('userId', '1')
+        axios.post('url', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'API-KEY': '22344'
+            },
+            withCredentials: true
+        })
+            .then(res => {
+                console.log('✅ Upload success', res.data)
+            })
+            .catch(err => {
+                console.error('❌ Upload error', err)
+            })
+            .finally(() => setDrag(false)); // ✅ drag убираем в finally
+    }
+
     return (
         <div className='app'>
             {drag
@@ -21,6 +44,7 @@ function App() {
                     onDragStart={(e => dragStartHundler(e))}
                     onDragLeave={(e => dragLeavetHundler(e))}
                     onDragOver={(e => dragStartHundler(e))}
+                    onDrop={(e => dropHundler(e))}
                     className='drop-area'>Отпусти файлы, чтобы загрузить их</div>
                 : <div
                     onDragStart={(e => dragStartHundler(e))}
